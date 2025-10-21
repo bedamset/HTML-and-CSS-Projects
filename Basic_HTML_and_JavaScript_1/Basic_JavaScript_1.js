86cfa5b02090d11f8e6aa5a2d2e44136819252ff
bedamset <pavankumarb8985189500@gmail.com>
2025-10-21 10:53:20 +0100

    Add Basic_HTML_and_JavaScript_1 as normal folder

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,1 @@
+alert("Hello, this is my JavaScript alert from an external file!");
---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
@@ -0,0 +1,5 @@
+function My_First_Function() {
+      var String = "Hello, It's October 21st 2025!";
+      var result = String.fontcolor("green");
+      document.getElementById("October").innerHTML = result;
+    }
